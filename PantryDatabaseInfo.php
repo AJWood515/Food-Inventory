1588e97d4dd5b00e91d892987cddea51b9dc7da5
AJWood515 <32825644+AJWood515@users.noreply.github.com>
2018-04-11 20:37:11 -0500

    changes made by alex

---
+++ PantryDatabaseInfo.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+
+<?php
+
+$host="foodpantrydatabase.ckyeqeyox4kj.us-east-2.rds.amazonaws.com";
+$port=3306;
+$socket="";
+$user="PantryAdmin";
+$password="Food1234";
+$dbname="Pantry";
+
+
+
+ ?>
+</html>
--- inventory.php
+++ inventory.php
@@ -1,63 +1,75 @@
-<!Doctype html>
-
+<!DOCTYPE html>
+<html>
 <?php
 include "PHP_FoodPantryDatabase_Connection.php";
 
-try{
-$query = "Select * from vegetables";
-//$result = $con->query($stmt);
 
 ?>
-<table border='1'>
-	<tr>
-		<td>ID</td>
-		<td>Vegetable</td>
-		<td>Stock</td>
-</table>
-<?php
+<head>
+<link rel="stylesheet" href="PREP.css" type="text/css" />
+<link rel="icon" href="images/PrepIcon.png" type="image/gif" sizes="16x16">
+</head>
+<body>
+<div id = "container">
+<header>
+<h1>Your Inventory</h1>
+<div class="header">
+</div>
 
-if ($stmt = $con->prepare($query)) {
-    $stmt->execute();
-    $stmt->bind_result($veggieID, $veggieName,$veggieStock);
-    while ($stmt->fetch()) {
-      echo "<tr>";
-        echo "<td>".$veggieID."</td>";
-        echo "<td>".$veggieName."</td>";
-        echo "<td>".$veggieStock."</td>";
-      echo "</tr>";
-    }
-    $stmt->close();
-}
-/*
-  while($result = $stmt-> fetchall()) {
-    echo "<tr>";
-      echo "<td>".$result['veggieID']."</td>";
-      echo "<td>".$result['name']."</td>";
-      echo "<td>".$result['Stock']."</td>";
-    echo "</tr>";
+<ul>
+  <li><a href="home.html">Home</a></li>
+  <li><a class="active">Inventory</a></li>
+</ul>
+
+</header>
+<aside>
+<p>Hello</p>
+</aside>
+<main>
+<p>
+  <table border='1'>
+	   <tr>
+		     <td>Name</td>
+	       <td>Quantity</td>
+         <td>Calories per Unit</td>
+         <td>Quantity of Units per Cup</td>
+         <td>Unit</td>
+         <td>Ingredient</td>
+
+
+  <?php
+  //try{
+    $query = "Select * from ingredients";
+
+
+  if ($stmt = $con->prepare($query)) {
+      $stmt->execute();
+      $stmt->bind_result($name, $quantity, $caloriesPerUnit, $quantityOfUnitsPerCup, $unit, $ingredients);
+      while ($stmt->fetch()) {
+        echo "<tr>";
+          echo "<td>".$name."</td>";
+          echo "<td>".$quantity."</td>";
+          echo "<td>".$caloriesPerUnit."</td>";
+          echo "<td>".$quantityOfUnitsPerCup."</td>";
+          echo "<td>".$unit."</td>";
+          echo "<td>".$ingredients."</td>";
+
+        echo "</tr>";
+      }
+      $stmt->close();
   }
-*/
-}
-catch (PDOException $e){
-  echo "Error: ". $e->getMessage();
-}
-$conn = null;
-?>
+  /*catch (PDOException $e){
+    echo "Error: ". $e->getMessage();
+  }*/
+  $conn = null;
 
-
-<!--
-
-$query = "Select * from vegetables";
-
-
-if ($stmt = $con->prepare($query)) {
-    $stmt->execute();
-    $stmt->bind_result($field1, $field2);
-    while ($stmt->fetch()) {
-        //printf("%s, %s\n", $field1, $field2);
-    }
-    $stmt->close();
-}
-*/
--->
+  ?>
+</table>
+</p>
+</main>
+<footer>
+<p>This site was created for educational purposes only</p>
+</footer>
+</div><!-- end div container --->
+</body>
 </html>
